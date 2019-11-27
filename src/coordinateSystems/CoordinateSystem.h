54724706cb51bd3c275d4ffedb59e37d0875451a
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2019-11-27 18:46:06 +0100

    change public interface of the coordinate system to be more useful

--- src/coordinateSystems/CoordinateSystem.h
+++ src/coordinateSystems/CoordinateSystem.h
@@ -25,8 +25,11 @@
  * class CoordinateSystem
  *
  * usage:
- * Base class for all coordinate systems. Provides coordinate conversions to/from cartesian coordinates and access to grid cells and face.
- * Also provides information about adjacent cells and faces.
+ * Base class for all coordinate systems. Provides coordinate conversions to/from cartesian
+ * coordinates and access to grid cells and face.
+ * Also provides information about adjacent cells.
+ * No direct support for staggered grid. As a convention each cell stores face/corner values belonging
+ * to its upper right.
  *
  */
 class CoordinateSystem
@@ -36,34 +39,37 @@ public:
     virtual ~CoordinateSystem() = default;
 
     // convert
-    virtual float3 getCartesian(float3 coord)=0; //!< converts a coordinate into cartesian coordinates
-    virtual float3 getCoord(float3 cartesian)=0; //!< converts cartesian coordinate into this coordinate system
+    virtual float3 getCartesian(const float3& coord) const =0; //!< converts a coordinate into cartesian coordinates
+    virtual float3 getCoord(const float3& cartesian) const =0; //!< converts cartesian coordinate into this coordinate system
 
     // for quantities stored at cell center
-    virtual float3 getCellCoordinate(int cellId)=0; //!< get the coordinates of a specific cell
-    virtual int getCellId(float3 coord)=0; //!< get the the cell id that belongs coordinates "coord"
-
-    // for quantities stored at cel faces
-    virtual float3 getFaceCoordinate(int faceId)=0; //!< get the coordinate of a cell face
-    virtual int getFaceId(float3 coord)=0; //!< get the cell face closest to coordinates "coord"
+    virtual float3 getCellCoordinate(int cellId) const =0; //!< get the coordinates of a specific cell
+    virtual float3 getCellCoordinate3d(const int3& cellId3d) const =0; //!< get the coordinates of the multi dimensional cell id
+    virtual int getCellId(const float3& coord) const =0; //!< get the the cell id that belongs coordinates "coord"
+    virtual int3 getCellId3d(const float3& coord) const =0; //!< get the multi dimensional cell id
 
     // adjacency
-    virtual int getRightFace(int cellId, int dimension)=0; //!< get face ids for a given cell id along axis dimension
-    virtual int getLeftFace(int cellId, int dimension)=0; //!< get face ids for a given cell id along axis dimension
-    virtual void getFaces(int cellId, int dimension, int& faceId1, int& faceId2)=0; //!< get face ids for a given cell id along axis dimension
-    virtual int getRightNeighbor(int cellId, int dimension)=0; //!< get neighbors for given cell along axis
-    virtual int getLightNeighbor(int cellId, int dimension)=0; //!< get neighbors for given cell along axis
-    virtual void getNeighbors(int cellId, int dimension, int& cellId1, int& cellId2)=0; //!< get neighbors for given cell along axis
+    virtual int getRightNeighbor(int cellId) const =0; //!< get neighbors for given cell along first positive axis
+    virtual int getLeftNeighbor(int cellId) const =0; //!< get neighbors for given cell along first negative axis
+    virtual int getForwardNeighbor(int cellId) const =0; //!< get neighbors for given cell along second positive axis
+    virtual int getBackwardNeighbor(int cellId) const =0; //!< get neighbors for given cell along negative axis
+    virtual int getUpNeighbor(int cellId) const =0; //!< get neighbors for given cell along third positive axis
+    virtual int getDownNeighbor(int cellId) const =0; //!< get neighbors for given cell along third megative axis
 
     // boundaries
-    virtual float3 getMinCoord()=0; //!< get the lower bound for all dimensions
-    virtual float3 getMaxCoord()=0; //!< get the upper bound for all dimensions
-    virtual float3 getDimension()=0; //!< get the number of dimensions
-    virtual float3 getCartesianDimension()=0; //!< get the number of dimensions in cartesian coordinates (eg surface of sphere dim=2 cartesian_dim = 3)
+    virtual float3 getMinCoord() const =0; //!< get the lower bound for all dimensions
+    virtual float3 getMaxCoord() const =0; //!< get the upper bound for all dimensions
+    virtual int numGridCells() const =0; //!< total number of grid cells
+    virtual int3 numGridCells3d() const =0; //!< number of grid cells in each dimension
+
+    // dimensions
+    virtual float3 getCellSize() const =0; //! get the size of the cell in target coordinates (uniform grid)
+    virtual int getDimension() const =0; //!< get the number of dimensions 1-3
+    virtual int getCartesianDimension() const =0; //!< get the number of dimensions in cartesian coordinates 1-3 (eg surface of sphere dim=2 cartesian_dim = 3)
 
     // bounding box
-    virtual float3 getAABBMin()=0; //!< get the lower left  bounding box corner in cartesian coords
-    virtual float3 getAABBMax()=0; //!< get the upper right bounding box corner in cartesian coords
+    virtual float3 getAABBMin() const =0; //!< get the lower left  bounding box corner in cartesian coords
+    virtual float3 getAABBMax() const =0; //!< get the upper right bounding box corner in cartesian coords
 };
 
 
