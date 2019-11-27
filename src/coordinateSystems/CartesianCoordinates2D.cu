85008d4f818b44dd57abbc5f4aa18524d160e5fd
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2019-11-27 21:16:21 +0100

    implement CartesianCoordinates2d

---
+++ src/coordinateSystems/CartesianCoordinates2D.cu
@@ -0,0 +1,141 @@
+/*
+ * CIRCULATION
+ * CartesianCoordinates2D.cpp
+ *
+ * @author: Hendrik Schwanekamp
+ * @mail:   hendrik.schwanekamp@gmx.net
+ *
+ * Implements the CartesianCoordinates2D class
+ *
+ * Copyright (c) 2019 Hendrik Schwanekamp
+ *
+ */
+
+// includes
+//--------------------
+#include "CartesianCoordinates2D.h"
+//--------------------
+
+// namespace
+//--------------------
+
+//--------------------
+
+// function definitions of the CartesianCoordinates2D class
+//-------------------------------------------------------------------
+CartesianCoordinates2D::CartesianCoordinates2D(float2 min, float2 max, int2 numGridCells)
+    : m_min(min), m_max(max), m_numGridCells(numGridCells),
+    m_totalNumGridCells(numGridCells.x*numGridCells.y),
+    m_size(max-min),
+    m_cellSize( m_size / make_float2(numGridCells))
+{
+}
+
+float3 CartesianCoordinates2D::getCartesian(const float3& coord) const
+{
+    return float3{coord.x,coord.y,0};
+}
+
+float3 CartesianCoordinates2D::getCoord(const float3& cartesian) const
+{
+    return float3{cartesian.x,cartesian.y,0};
+}
+
+float3 CartesianCoordinates2D::getCellCoordinate(int cellId) const
+{
+    return getCellCoordinate3d({cellId%m_numGridCells.x, cellId/m_numGridCells.x,0});
+}
+
+float3 CartesianCoordinates2D::getCellCoordinate3d(const int3& cellId3d) const
+{
+    int2 cellId2d = make_int2(cellId3d);
+    float2 coord2d = make_float2(cellId2d) * m_cellSize + m_min;
+    return make_float3(coord2d);
+}
+
+int CartesianCoordinates2D::getCellId(const float3& coord) const
+{
+    int3 cellId3d = getCellId3d(coord);
+    return cellId3d.y*m_numGridCells.x+ cellId3d.x;
+}
+
+int3 CartesianCoordinates2D::getCellId3d(const float3& coord) const
+{
+    float2 coord2d =  (make_float2(coord) - m_min) / m_cellSize;
+    return make_int3(rintf(coord2d.x),rintf(coord2d.y),0);
+}
+
+int CartesianCoordinates2D::getRightNeighbor(int cellId) const
+{
+    return cellId+1;
+}
+
+int CartesianCoordinates2D::getLeftNeighbor(int cellId) const
+{
+    return cellId-1;
+}
+
+int CartesianCoordinates2D::getForwardNeighbor(int cellId) const
+{
+    return cellId+m_numGridCells.x;
+}
+
+int CartesianCoordinates2D::getBackwardNeighbor(int cellId) const
+{
+    return cellId-m_numGridCells.x;
+}
+
+int CartesianCoordinates2D::getUpNeighbor(int cellId) const
+{
+    return -1;
+}
+
+int CartesianCoordinates2D::getDownNeighbor(int cellId) const
+{
+    return -1;
+}
+
+int CartesianCoordinates2D::numGridCells() const
+{
+    return m_totalNumGridCells;
+}
+
+int3 CartesianCoordinates2D::numGridCells3d() const
+{
+    return make_int3(m_numGridCells);
+}
+
+float3 CartesianCoordinates2D::getCellSize() const
+{
+    return make_float3(m_cellSize);
+}
+
+float3 CartesianCoordinates2D::getMinCoord() const
+{
+    return float3{m_min.x, m_min.y, 0};
+}
+
+float3 CartesianCoordinates2D::getMaxCoord() const
+{
+    return float3{m_max.x,m_max.y,0};
+}
+
+int CartesianCoordinates2D::getDimension() const
+{
+    return 2;
+}
+
+int CartesianCoordinates2D::getCartesianDimension() const
+{
+    return 2;
+}
+
+float3 CartesianCoordinates2D::getAABBMin() const
+{
+    return float3{m_min.x, m_min.y, 0};
+}
+
+float3 CartesianCoordinates2D::getAABBMax() const
+{
+    return float3{m_max.x,m_max.y,0};
+}
---
+++ src/coordinateSystems/CartesianCoordinates2D.h
@@ -0,0 +1,79 @@
+/*
+ * CIRCULATION
+ * CartesianCoordinates2D.h
+ *
+ * @author: Hendrik Schwanekamp
+ * @mail:   hendrik.schwanekamp@gmx.net
+ *
+ * Implements the CartesianCoordinates2D class
+ *
+ * Copyright (c) 2019 Hendrik Schwanekamp
+ *
+ */
+
+#ifndef CIRCULATION_CARTESIANCOORDINATES2D_H
+#define CIRCULATION_CARTESIANCOORDINATES2D_H
+
+// includes
+//--------------------
+#include "CoordinateSystem.h"
+//--------------------
+
+//-------------------------------------------------------------------
+/**
+ * class CartesianCoordinates2D
+ *
+ * 2D cartesian grid in the x-y-plane. Grid cell access is row major.
+ * No bounds checking is done!
+ *
+ */
+class CartesianCoordinates2D : public CoordinateSystem
+{
+public:
+    CartesianCoordinates2D(float2 min, float2 max, int2 numGridCells); //!< smallest value, biggest value and number of grid cells in each dimension
+    ~CartesianCoordinates2D() override = default;
+
+    // convert
+    float3 getCartesian(const float3& coord) const override; //!< converts a coordinate into cartesian coordinates
+    float3 getCoord(const float3& cartesian) const override; //!< converts cartesian coordinate into this coordinate system
+
+    // for quantities stored at cell center
+    float3 getCellCoordinate(int cellId) const override; //!< get the coordinates of a specific cell
+    float3 getCellCoordinate3d(const int3& cellId3d) const override; //!< get the coordinates of the multi dimensional cell id
+    int getCellId(const float3& coord) const override; //!< get the the cell id that belongs coordinates "coord"
+    int3 getCellId3d(const float3& coord) const override; //!< get the multi dimensional cell id
+
+    // adjacency
+    int getRightNeighbor(int cellId) const override; //!< get neighbors for given cell along first positive axis
+    int getLeftNeighbor(int cellId) const override; //!< get neighbors for given cell along first negative axis
+    int getForwardNeighbor(int cellId) const override; //!< get neighbors for given cell along second positive axis
+    int getBackwardNeighbor(int cellId) const override; //!< get neighbors for given cell along negative axis
+    int getUpNeighbor(int cellId) const override; //!< get neighbors for given cell along third positive axis
+    int getDownNeighbor(int cellId) const override; //!< get neighbors for given cell along third megative axis
+
+    // boundaries
+    float3 getMinCoord() const override; //!< get the lower bound for all dimensions
+    float3 getMaxCoord() const override; //!< get the upper bound for all dimensions
+    int numGridCells() const override; //!< total number of grid cells
+    int3 numGridCells3d() const override; //!< number of grid cells in each dimension
+
+    // dimensions
+    float3 getCellSize() const override; //! get the size of the cell in target coordinates (uniform grid)
+    int getDimension() const override; //!< get the number of dimensions
+    int getCartesianDimension() const override; //!< get the number of dimensions in cartesian coordinates (eg surface of sphere dim=2 cartesian_dim = 3)
+
+    // bounding box
+    float3 getAABBMin() const override; //!< get the lower left  bounding box corner in cartesian coords
+    float3 getAABBMax() const override; //!< get the upper right bounding box corner in cartesian coords
+
+private:
+    const float2 m_min; //!< smallest possible coordinate
+    const float2 m_max; //!< highest possible coordinate
+    const float2 m_size; //!< m_max - m_min
+    const float2 m_cellSize; //!< size of one grid cell
+    const int2 m_numGridCells; //!< number of cells in each dimension
+    const int m_totalNumGridCells; //!< total number of cells
+};
+
+
+#endif //CIRCULATION_CARTESIANCOORDINATES2D_H
