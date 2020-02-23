5a598bf4a7dc7120b50605a384b113f95764b95e
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2020-02-23 10:50:39 +0100

    boundary handling for offset values

--- src/boundaryConditions.h
+++ src/boundaryConditions.h
@@ -53,14 +53,14 @@ void initializeFixedValueBoundaries(bool boundX, bool boundY, const T& valueX, c
 }
 
 template < AT attributeType, typename csT, typename gridRefT>
-__global__ void mirrorBoundariesGPU(int numBoundCellsY, int  numBoundCellsX, csT coordinateSystem, gridRefT grid)
+__global__ void mirrorBoundariesGPU(int numBoundCellsY, int  numBoundCellsX, csT coordinateSystem, gridRefT grid, bool isOffset)
 {
     csT cs = coordinateSystem;
 
     for(int i : mpu::gridStrideRange(numBoundCellsY))
     {
         // transform boundary cell id into actual cell id
-        int3 cellId3d{i % cs.getNumGridCells3d().x, 0, 0};
+        int3 cellId3d{i % cs.getNumGridCells3d().x, isOffset ? 1 : 0, 0};
         if(i >= cs.getNumGridCells3d().x)
             cellId3d.y = cs.getNumGridCells3d().y - 1;
         int cellId = cs.getCellId(cellId3d);
@@ -74,7 +74,8 @@ __global__ void mirrorBoundariesGPU(int numBoundCellsY, int  numBoundCellsX, csT
     for(int i : mpu::gridStrideRange(numBoundCellsX))
     {
         // transform boundary cell id into actual cell id
-        int3 cellId3d{(i % 2) * (cs.getNumGridCells3d().x - 1), 1 + i / 2, 0};
+        int offset = isOffset ? 2 : 1;
+        int3 cellId3d{(i % 2) * (cs.getNumGridCells3d().x - offset), 1 + i / 2, 0};
         int cellId = cs.getCellId(cellId3d);
 
         int neighbourId = (cellId3d.x == 0) ? cs.getRightNeighbor(cellId) : cs.getLeftNeighbor(cellId);
@@ -88,7 +89,7 @@ __global__ void mirrorBoundariesGPU(int numBoundCellsY, int  numBoundCellsX, csT
  * @brief update the boundaries on the grid to mirror the closest valid value
  */
 template < AT attributeType, typename csT, typename gridT>
-void handleMirroredBoundaries(bool boundX, bool boundY, csT& cs, gridT& grid)
+void handleMirroredBoundaries(bool boundX, bool boundY, csT& cs, gridT& grid, bool isOffset = false)
 {
     int numBoundCellsY = boundY ? 2 * cs.hasBoundary().y * cs.getNumGridCells3d().x : 0;
     int numBoundCellsX = boundX ? 2 * cs.hasBoundary().x * cs.getNumGridCells3d().y - 4 : 0;
@@ -97,7 +98,7 @@ void handleMirroredBoundaries(bool boundX, bool boundY, csT& cs, gridT& grid)
     dim3 numBlocks{ static_cast<unsigned int>(mpu::numBlocks( min(numBoundCellsX,numBoundCellsY) ,blocksize.x)),
                     1, 1};
 
-    mirrorBoundariesGPU<attributeType><<<numBlocks, blocksize>>>(numBoundCellsY,numBoundCellsX,cs,grid.getGridReference());
+    mirrorBoundariesGPU<attributeType><<<numBlocks, blocksize>>>(numBoundCellsY,numBoundCellsX,cs,grid.getGridReference(), isOffset);
 }
 
 #endif //CIRCULATION_BOUNDARYCONDITIONS_H
